4ff1f08f03aeab813daf9498f82deaaafdcd9e0e
Andrew Peterson <andrewpeterson1968@gmail.com>
2022-11-17 17:49:30 -0700

    Finished Login screen

---
+++ constants/ColorStyle.js
@@ -0,0 +1,15 @@
+export const GlobalColors = {
+  colors: {
+    lightGreen: "	#90EE90",
+    limeGreen: "	#32CD32",
+    niceCream: '#fefaec', 
+    redish: '#f38181',
+    blueish: '#625772'
+  },
+  logo: {
+    twitblue: '#118DF0',
+    FBblue: '#0e2f56',
+    PintRed: '#ff304f',
+    Subscribe: '#3fc1c9'
+  },
+};
